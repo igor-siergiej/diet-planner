1a27a64fc4e65a78b60b4ab8644583844ecee229
Igor Siergiej <igorsiergiej@gmail.com>
2021-05-01 14:38:34 +0100

    test commit

--- src/diet-planner/src/com/app/planner/Main.java
+++ src/diet-planner/src/com/app/planner/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
-        //test commit
+        //test commit 2
     }
 
 
