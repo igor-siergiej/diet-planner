a57028a4303f24f34f8e3efd4bd7f1d3a94ca05b
igs1 <igs1@aber.ac.uk>
2021-05-01 14:27:58 +0100

    Got changing scenes to work

--- src/diet-planner/src/com/app/planner/mainscreencontroller/MainScreenController.java
+++ src/diet-planner/src/com/app/planner/mainscreencontroller/MainScreenController.java
@@ -1,4 +1,24 @@
 package com.app.planner.mainscreencontroller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class MainScreenController {
+
+    public void goToConfiguration(ActionEvent event) throws IOException {
+        Parent configScreenParent = FXMLLoader.load(getClass().getResource("../configcontroller/configScreen.fxml"));
+        Scene configScreenScene = new Scene(configScreenParent);
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        window.setScene(configScreenScene);
+        window.show();
+
+        System.out.println("works");
+    }
 }
