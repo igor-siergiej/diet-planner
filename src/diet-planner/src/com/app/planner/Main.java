08402eeec1d2c5684b542c5aea305167ce417038
Igor Siergiej <igorsiergiej@gmail.com>
2021-05-02 07:48:43 +0100

    working connection to localhost postgresql database

--- src/diet-planner/src/com/app/planner/Main.java
+++ src/diet-planner/src/com/app/planner/Main.java
@@ -1,5 +1,6 @@
 package com.app.planner;
 
+import com.app.planner.util.DatabaseConnection;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -18,6 +19,8 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
+        DatabaseConnection db = new DatabaseConnection();
+        db.testConnection();
         launch(args);
     }
 }
