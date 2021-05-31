7a35c6faae600a8549c2954bdbda263825b631fc
Igor Siergiej <igorsiergiej@gmail.com>
2021-05-31 22:36:10 +0100

    updated dataset format and implemented reading JSON from file to Food object

--- src/diet-planner/src/com/app/planner/util/GsonStorage.java
+++ src/diet-planner/src/com/app/planner/util/GsonStorage.java
@@ -3,14 +3,27 @@ package com.app.planner.util;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+
 public class GsonStorage {
     public void saveObjectToJson() {
-        Food pizza = new Food();
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        String json = gson.toJson(pizza);
-        System.out.println(json);
 
-        Food burger = gson.fromJson(json, Food.class);
-        System.out.println(burger.toString());
+        try {
+            // reads the entire array in the file
+            Reader reader = Files.newBufferedReader(Paths.get("test.json"));
+            Food burger = gson.fromJson(reader, Food.class);
+            System.out.println(burger.toString());
+
+        } catch (FileNotFoundException e) {
+            System.out.println("WARNING: History File Not Found.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
     }
 }
---
+++ src/diet-planner/src/com/app/planner/util/Nutrient.java
@@ -0,0 +1,19 @@
+package com.app.planner.util;
+
+public class Nutrient {
+    private String nutrientName;
+    private float nutrientValue;
+
+    public Nutrient(String nutrientName, float nutrientValue) {
+        this.nutrientName = nutrientName;
+        this.nutrientValue = nutrientValue;
+    }
+
+    @Override
+    public String toString() {
+        return "Nutrient{" +
+                "nutrientName='" + nutrientName + '\'' +
+                ", nutrientValue=" + nutrientValue +
+                '}';
+    }
+}
