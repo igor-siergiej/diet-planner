6de4df9210f3ed4b900e28e8b32d0cdac1bdd1d2
Igor Siergiej <igorsiergiej@gmail.com>
2021-06-16 16:48:14 +0100

    Implemented setting profile name

--- src/diet-planner/src/com/app/planner/mainscreencontroller/MainScreenController.java
+++ src/diet-planner/src/com/app/planner/mainscreencontroller/MainScreenController.java
@@ -1,13 +1,17 @@
 package com.app.planner.mainscreencontroller;
 
+import com.app.planner.Diary;
 import com.app.planner.Main;
 import com.app.planner.Profile;
 import com.app.planner.profilescreencontroller.ProfileScreenController;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import javax.swing.*;
@@ -16,6 +20,12 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class MainScreenController {
+
+    @FXML
+    private Pane enterProfilePane;
+
+    @FXML
+    private TextField enterProfileNameTextField;
 
     public void goToConfiguration(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/com/app/planner/configcontroller/configScreen.fxml"));
@@ -26,21 +36,23 @@ public class MainScreenController {
         window.show();
     }
 
-    public void createNewProfile(ActionEvent event) throws IOException {
-        Profile profile = new Profile();
-
-        profileHelper(event, profile);
+    public void createNewProfile() {
+        enterProfilePane.setVisible(true);
     }
 
-    public void loadProfile(ActionEvent event) throws IOException, ClassNotFoundException, UnsupportedLookAndFeelException, InstantiationException, IllegalAccessException {
-        Main main = new Main();
-        Profile profile = new Profile();
-        profile.loadFromFile(main.chooseFile("load"));
-
-        profileHelper(event, profile);
+    public void setEnterProfilePaneInvisible() {
+        enterProfilePane.setVisible(false);
     }
 
-    private void profileHelper(ActionEvent event, Profile profile) throws IOException {
+    public void setProfile(ActionEvent event) throws IOException {
+        Profile profile = new Profile();
+        Diary diary = new Diary();
+        profile.setDiary(diary);
+        profile.setProfileName(enterProfileNameTextField.getText());
+        goToProfileScreen(event,profile);
+    }
+
+    private void goToProfileScreen(ActionEvent event, Profile profile) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/app/planner/profilescreencontroller/profileScreen.fxml"));
         Parent root = loader.load();
 
@@ -53,4 +65,14 @@ public class MainScreenController {
         window.setScene(scene);
         window.show();
     }
+
+    public void loadProfile(ActionEvent event) throws IOException, ClassNotFoundException, UnsupportedLookAndFeelException, InstantiationException, IllegalAccessException {
+        Main main = new Main();
+        Profile profile = new Profile();
+        profile.loadFromFile(main.chooseFile("load"));
+
+
+        goToProfileScreen(event, profile);
+    }
+
 }
