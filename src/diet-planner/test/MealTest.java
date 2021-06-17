92f5cd508d63b37b040780f898e83a27b755a6ac
Igor Siergiej <igorsiergiej@gmail.com>
2021-06-17 12:40:25 +0100

    Wrote tests for Meal Class

---
+++ src/diet-planner/test/MealTest.java
@@ -0,0 +1,51 @@
+import com.app.planner.Food;
+import com.app.planner.Meal;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.stream.JsonReader;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MealTest {
+
+    private Meal meal;
+
+    @Before
+    public void setUp() {
+        meal = new Meal();
+        ArrayList<Food> returnList = new ArrayList<>();
+        // reads the testData.json file which contains the first 3 foods from the database
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        InputStream in = getClass().getResourceAsStream("testData.json");
+        JsonReader reader = new JsonReader(new InputStreamReader(in));
+        Food[] foods = gson.fromJson(reader,Food[].class);
+        List<Food> foodsList = Arrays.asList(foods);
+        returnList.addAll(foodsList);
+        meal.addFoods(returnList,new int[]{200,300,100}); //These are the testing portions
+        System.out.println(meal.toString());
+    }
+
+    @Test
+    public void testGetFoods() {
+        assertEquals("Size of the foods should be 3",3, meal.getFoods().size());
+
+        assertEquals("Size of the nutrients should be 53",53,meal.getFoods().get(0).getNutrients().size());
+    }
+
+    @Test
+    public void testNutrientValues() {
+        assertEquals(153.4 ,meal.getFoods().get(0).getNutrients().get(0).getNutrientValue(),0.0001);
+        //this tests the amount of water in 200 grams of Ackee
+        assertEquals(0.78 ,meal.getFoods().get(1).getNutrients().get(1).getNutrientValue(),0.0001);
+        //this tests the amount of nitrogen in 300 grams of Agar
+        assertEquals(0.2 ,meal.getFoods().get(2).getNutrients().get(2).getNutrientValue(),0.0001);
+        //this tests the amount of protein in 100 grams of Agar (soaked and drained)
+    }
+}
