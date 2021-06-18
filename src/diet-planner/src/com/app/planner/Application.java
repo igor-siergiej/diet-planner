b64bebbf8d253d4a02e0777a7fb2827410114772
Igor Siergiej <igorsiergiej@gmail.com>
2021-06-18 21:32:35 +0100

    Implemented register and login methods for DB

--- src/diet-planner/src/com/app/planner/Application.java
+++ src/diet-planner/src/com/app/planner/Application.java
@@ -10,49 +10,6 @@ import java.util.Enumeration;
 
 public class Application {
     public static void main(String[] args) throws IOException {
-
-        /*Main main = new Main();
-        DatabaseConnection db = new DatabaseConnection();
-        db.testConnection();
-
-        Profile profile = new Profile();
-        ArrayList<Food> data = main.initialiseData();
-        System.out.println(data.toString());
-
-        new File("profiles").mkdirs();
-
-        profile.loadFromFile(main.chooseFile("load"));
-        System.out.println(profile.toString());
-
-
-
-
-
-        Meal meal = new Meal();
-        meal.setMealName("ciabatta");
-        meal.addFoods(main.sortedFoodSearch(data,"tomatoes"));
-
-        Entry entry = new Entry(meal, LocalDateTime.now(),EntryType.BREAKFAST);
-        Diary diary = new Diary();
-        diary.addEntry(entry);
-        diary.addEntry(entry);
-
-        ArrayList<Nutrient> nutrientArrayList = new ArrayList<>();
-        for (Entry entry5: diary.getEntriesDay(LocalDateTime.now().getDayOfMonth())) {
-            for (Food food: entry5.getMeal().getFoods()) {
-                nutrientArrayList.addAll(food.getNutrients());
-            }
-        }
-
-        System.out.println(nutrientArrayList.toString());
-
-        System.out.println(main.combineNutrientList(nutrientArrayList).toString());
-
-        profile.setDiary(diary);
-        profile.setProfileName("profile1");
-        File file = main.chooseFile("save");
-        profile.saveToFile(file);*/
-
         Main.main(args);
     }
 }
