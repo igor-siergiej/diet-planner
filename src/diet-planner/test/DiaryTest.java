ab7e74d884044daaa74e2482eb0aff06b6f20e81
Igor Siergiej <igorsiergiej@gmail.com>
2021-06-18 13:49:34 +0100

    Wrote another test for Diary Class

--- src/diet-planner/test/DiaryTest.java
+++ src/diet-planner/test/DiaryTest.java
@@ -24,16 +24,19 @@ public class DiaryTest {
         LocalDateTime localDateTime2 = LocalDateTime.parse("2021-06-16T06:30:00");
         LocalDateTime localDateTime3 = LocalDateTime.parse("2021-06-17T06:30:00");
         LocalDateTime localDateTime4 = LocalDateTime.parse("2021-06-01T06:30:00");
+        LocalDateTime localDateTime5 = LocalDateTime.parse("2021-06-02T06:30:00");
         Entry entry = new Entry(new Meal(),localDateTime, EntryType.BREAKFAST);
         Entry entry1 = new Entry(new Meal(),localDateTime1, EntryType.BREAKFAST);
         Entry entry2 = new Entry(new Meal(),localDateTime2, EntryType.BREAKFAST);
         Entry entry3 = new Entry(new Meal(),localDateTime3, EntryType.BREAKFAST);
         Entry entry4 = new Entry(new Meal(),localDateTime4, EntryType.BREAKFAST);
+        Entry entry5 = new Entry(new Meal(),localDateTime5, EntryType.BREAKFAST);
         diary.addEntry(entry);
         diary.addEntry(entry1);
         diary.addEntry(entry2);
         diary.addEntry(entry3);
         diary.addEntry(entry4);
+        diary.addEntry(entry5);
     }
 
     @Test
@@ -45,6 +48,6 @@ public class DiaryTest {
     @Test
     public void testGetEntriesWeek() {
         assertEquals("Size of arrayList should be 4",4,diary.getEntriesWeek(15).size());
-        assertEquals("Size of arrayList should be 1",1,diary.getEntriesWeek(1).size());
+        assertEquals("Size of arrayList should be 2",2,diary.getEntriesWeek(1).size());
     }
 }
