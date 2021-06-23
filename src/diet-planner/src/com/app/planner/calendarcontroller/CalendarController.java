1c0869f2b268f832c3189e87d4d8164f608f6d75
Igor Siergiej <igorsiergiej@gmail.com>
2021-06-23 22:04:49 +0100

    Calendar now displays month name and day numbers

--- src/diet-planner/src/com/app/planner/calendarcontroller/CalendarController.java
+++ src/diet-planner/src/com/app/planner/calendarcontroller/CalendarController.java
@@ -1,23 +1,67 @@
 package com.app.planner.calendarcontroller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+import javax.annotation.Resources;
 import java.io.IOException;
+import java.net.URL;
+import java.time.LocalDate;
+import java.time.YearMonth;
 
 public class CalendarController {
+    private LocalDate localDate = LocalDate.now();
+    private YearMonth yearMonth = YearMonth.of(localDate.getYear(), localDate.getMonth().getValue());
 
-    public void goToConfiguration(ActionEvent event) throws IOException {
-        Parent configScreenParent = FXMLLoader.load(getClass().getResource("../configcontroller/configScreen.fxml"));
-        Scene configScreenScene = new Scene(configScreenParent);
-        configScreenScene.getStylesheets().add("com/app/planner/style.css");
-        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
-        window.setScene(configScreenScene);
-        window.show();
+    @FXML
+    private Label monthLabel;
+
+    @FXML
+    private GridPane calendar;
+
+    @FXML
+    protected void initialize() {
+        populateCalendar(yearMonth);
+    }
+
+    public void populateCalendar(YearMonth yearMonth1) {
+        // Get the date we want to start with on the calendar
+        LocalDate calendarDate = LocalDate.of(yearMonth1.getYear(), yearMonth1.getMonthValue(), 1);
+        while (!calendarDate.getDayOfWeek().toString().equals("MONDAY") ) {
+            calendarDate = calendarDate.minusDays(1);
+        }
+        // TO-DO implement adding day numbers to calendar gridPane
+
+        calendar.getChildren().retainAll(calendar.getChildren().get(0));
+
+        for (int i = 1; i <6;i++) {
+            for (int j = 0; j < 7; j++) {
+                Label label = new Label();
+                label.setText(String.valueOf(calendarDate.getDayOfMonth()));
+                calendar.add(label,j,i);
+                calendarDate = calendarDate.plusDays(1);
+            }
+        }
+
+        monthLabel.setText(yearMonth1.getMonth().toString() + " " + String.valueOf(yearMonth1.getYear()));
+    }
+
+    public void nextMonth() {
+        yearMonth = yearMonth.plusMonths(1);
+        populateCalendar(yearMonth);
+    }
+
+    public void previousMonth() {
+        yearMonth = yearMonth.minusMonths(1);
+        populateCalendar(yearMonth);
     }
 
 
--- src/diet-planner/src/com/app/planner/profilescreencontroller/ProfileScreenController.java
+++ src/diet-planner/src/com/app/planner/profilescreencontroller/ProfileScreenController.java
@@ -1,8 +1,16 @@
 package com.app.planner.profilescreencontroller;
 
 import com.app.planner.Profile;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 
 public class ProfileScreenController {
@@ -12,6 +20,15 @@ public class ProfileScreenController {
     @FXML
     private TextArea profileDataTextArea;
 
+    public void goToCalendarScreen(ActionEvent event) throws IOException {
+        Parent mainScreenParent = FXMLLoader.load(getClass().getResource("/com/app/planner/calendarcontroller/calendar.fxml"));
+        Scene mainScreenScene = new Scene(mainScreenParent);
+        mainScreenScene.getStylesheets().add("com/app/planner/style.css");
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        window.setScene(mainScreenScene);
+        window.show();
+    }
+
     public void getProfile(Profile profile) {
         this.profile = profile;
     }
@@ -19,6 +36,8 @@ public class ProfileScreenController {
     public void showProfileData() {
         profileDataTextArea.setText(profile.toString());
     }
+
+
 
 
 }
