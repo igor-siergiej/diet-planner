d986ec63ec92a14e17be38648611f25618199cbc
Igor Siergiej <igorsiergiej@gmail.com>
2021-07-13 12:08:43 +0100

    Implemented db connection

--- src/diet-planner/src/com/app/planner/DatabaseConnection.java
+++ src/diet-planner/src/com/app/planner/DatabaseConnection.java
@@ -6,17 +6,17 @@ import java.sql.*;
 
 public class DatabaseConnection {
 
-    private static final String SQL_INSERT = "INSERT INTO users (id, password) VALUES (?,?)";
+    private static final String SQL_INSERT = "INSERT INTO users (username, password) VALUES (?,?)";
     private static final String SQL_SELECT = "SELECT * FROM users WHERE id = ? and password = ?";
 
     public void displayTable() {
-        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/test", "postgres", "")) {
+        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://ec2-54-74-60-70.eu-west-1.compute.amazonaws.com/d7p8thbvo4s0gr", "tehdblxcjrcior", "26bb184833b93b3bd5e568ddf5a93d07c3b55af476f6b0f3424f55f80d58f5d8")) {
             System.out.println("Connected to PostgreSQL database!");
             Statement statement = connection.createStatement();
-            System.out.printf("%-30.30s  %-30.30s%n", "id", "password");
+            System.out.printf("%-30.30s  %-30.30s%n", "username", "password");
             ResultSet resultSet = statement.executeQuery("SELECT * FROM users");
             while (resultSet.next()) {
-                System.out.printf("%-30.30s  %-30.30s%n", resultSet.getString("id"), resultSet.getString("password"));
+                System.out.printf("%-30.30s  %-30.30s%n", resultSet.getString("username"), resultSet.getString("password"));
             }
 
         } catch (SQLException e) {
@@ -25,12 +25,12 @@ public class DatabaseConnection {
         }
     }
 
-    public boolean register(String id, String pass) {
-        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/test", "postgres", "")) {
+    public boolean register(String username, String password) {
+        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://ec2-54-74-60-70.eu-west-1.compute.amazonaws.com/d7p8thbvo4s0gr", "tehdblxcjrcior", "26bb184833b93b3bd5e568ddf5a93d07c3b55af476f6b0f3424f55f80d58f5d8")) {
             PreparedStatement preparedStatement = connection.prepareStatement(SQL_INSERT); {
 
-                preparedStatement.setString(1, id);
-                preparedStatement.setString(2, pass);
+                preparedStatement.setString(1, username);
+                preparedStatement.setString(2, password);
 
         }
             preparedStatement.executeUpdate();
@@ -43,7 +43,7 @@ public class DatabaseConnection {
     }
 
     public boolean login(String id, String pass) {
-        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/test", "postgres", "")) {
+        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://ec2-54-74-60-70.eu-west-1.compute.amazonaws.com/d7p8thbvo4s0gr", "tehdblxcjrcior", "26bb184833b93b3bd5e568ddf5a93d07c3b55af476f6b0f3424f55f80d58f5d8")) {
             PreparedStatement preparedStatement = connection.prepareStatement(SQL_SELECT); {
 
                 preparedStatement.setString(1, id);
