80cbe3207c9e4222892698ce6b34841397c5d7f7
Igor Siergiej <igorsiergiej@gmail.com>
2021-07-13 12:56:36 +0100

    Added test files and added test data to db

---
+++ src/diet-planner/test/com/app/planner/DatabaseConnectionTest.java
@@ -0,0 +1,16 @@
+package com.app.planner;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DatabaseConnectionTest {
+
+    @Test
+    void register() {
+    }
+
+    @Test
+    void login() {
+    }
+}
---
+++ src/diet-planner/test/com/app/planner/PasswordUtilsTest.java
@@ -0,0 +1,24 @@
+package com.app.planner;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PasswordUtilsTest {
+
+    @Test
+    void getSalt() {
+    }
+
+    @Test
+    void hash() {
+    }
+
+    @Test
+    void generateSecurePassword() {
+    }
+
+    @Test
+    void verifyUserPassword() {
+    }
+}
