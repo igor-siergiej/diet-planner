61d753e8a49a781acda5ed66754f3f0687e72787
Igor Siergiej <igorsiergiej@gmail.com>
2021-07-15 18:23:55 +0100

    Kaj's setup

--- src/diet-planner/src/com/app/planner/Application.java
+++ src/diet-planner/src/com/app/planner/Application.java
@@ -1,12 +1,6 @@
 package com.app.planner;
 
-import com.app.planner.mainscreencontroller.MainScreenController;
-
-import java.io.File;
 import java.io.IOException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.Enumeration;
 
 public class Application {
     public static void main(String[] args) throws IOException {
