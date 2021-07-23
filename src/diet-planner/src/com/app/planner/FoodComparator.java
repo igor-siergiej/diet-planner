ca242b56b1cd38168407ca63e222de67f610712b
Igor Siergiej <igorsiergiej@gmail.com>
2021-07-23 13:47:55 +0100

    Improving code quality

--- src/diet-planner/src/com/app/planner/FoodComparator.java
+++ src/diet-planner/src/com/app/planner/FoodComparator.java
@@ -18,7 +18,8 @@ public class FoodComparator implements Comparator<Food> {
     }
 
     public static double similarity(String s1, String s2) {
-        String longer = s1, shorter = s2;
+        String longer = s1;
+        String shorter = s2;
         if (s1.length() < s2.length()) { // longer should always have greater length
             longer = s2; shorter = s1;
         }
