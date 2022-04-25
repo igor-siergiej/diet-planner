18e7ade6cc806f7b43056ffaa9dd714363cd8f0a
igor-siergiej <igorsiergiej@gmail.com>
2022-04-25 13:51:20 +0100

    More updates to project structure

--- src/diet-planner/src/com/app/planner/EntryType.java
+++
@@ -1,8 +0,0 @@
-package com.app.planner;
-
-public enum EntryType { // TODO later sort entries based on this type
-    BREAKFAST,
-    DINNER,
-    LUNCH,
-    SNACK
-}
--- src/diet-planner/src/com/app/planner/ThemeType.java
+++
@@ -1,7 +0,0 @@
-package com.app.planner;
-
-public enum ThemeType {  //TODO create more themes and more CSS files
-    Dark,
-    Light,
-
-}
