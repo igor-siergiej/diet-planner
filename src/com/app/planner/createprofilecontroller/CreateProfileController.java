6799a5d5fb44ed40755427e0aef8daf7ebeee62a
igor-siergiej <igorsiergiej@gmail.com>
2022-05-27 16:15:24 +0100

    Added input validation for CreateProfileController form

--- src/com/app/planner/createprofilecontroller/CreateProfileController.java
+++ src/com/app/planner/createprofilecontroller/CreateProfileController.java
@@ -1,22 +1,18 @@
 package com.app.planner.createprofilecontroller;
 
 import com.app.planner.BaseScreenController;
+import com.app.planner.InputValidation;
 import com.app.planner.Profile;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.RadioButton;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.TextField;
 
-import java.awt.event.ActionEvent;
 
 public class CreateProfileController extends BaseScreenController {
 
     Profile profile;
-
-    @FXML
-    private RadioButton maleRadioButton;
 
     @FXML
     private CheckBox pregnantCheckBox;
@@ -27,11 +23,42 @@ public class CreateProfileController extends BaseScreenController {
     @FXML
     private RadioButton femaleRadioButton;
 
-    public void initialiseCredentials(String email, String password) {
+    @FXML
+    private TextField ageTextField;
+
+    @FXML
+    private TextField heightTextField;
+
+    @FXML
+    private TextField weightTextField;
+
+    public void initialise(String email, String password) {
         this.profile = new Profile();
         this.profile.setEmail(email);
         this.profile.setPassword(password);
+        setAgeTextFieldEventHandler();
+        setHeightTextFieldEventHandler();
+        setWeightTextFieldEventHandler();
     }
+
+    public void setAgeTextFieldEventHandler() {
+        ageTextField.textProperty().addListener((observable, oldValue, newValue) -> {
+            ageTextField.setText(InputValidation.ageValidation(newValue));
+        });
+    }
+
+    public void setHeightTextFieldEventHandler() {
+        heightTextField.textProperty().addListener((observable, oldValue, newValue) -> {
+            heightTextField.setText(InputValidation.heightValidation(newValue));
+        });
+    }
+
+    public void setWeightTextFieldEventHandler() {
+        weightTextField.textProperty().addListener((observable, oldValue, newValue) -> {
+            weightTextField.setText(InputValidation.weightValidation(newValue));
+        });
+    }
+
 
     public void handleFemaleCheckBoxes() {
         if (femaleRadioButton.isSelected()) {
@@ -43,5 +70,11 @@ public class CreateProfileController extends BaseScreenController {
             pregnantCheckBox.setDisable(true);
             breastfeedingCheckBox.setDisable(true);
         }
+    }
+
+    public void createNewProfile(ActionEvent event) {
+        // get values from UI and add them to profile
+
+        goToProfileScreen(event,this.profile);
     }
 }
