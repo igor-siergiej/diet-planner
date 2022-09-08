f269365d0dff99962a01425523b4c165275d8b8d
igor-siergiej <igorsiergiej@gmail.com>
2022-09-08 17:21:25 +0100

    Added initial calorie graph to GraphViewScreen.fxmlc

--- src/com/app/planner/graphviewcontroller/GraphViewController.java
+++ src/com/app/planner/graphviewcontroller/GraphViewController.java
@@ -1,12 +1,15 @@
 package com.app.planner.graphviewcontroller;
 
 import com.app.planner.BaseScreenController;
+import com.app.planner.Entry;
 import javafx.fxml.FXML;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
+
+import java.util.ArrayList;
 
 public class GraphViewController extends BaseScreenController {
 
@@ -28,33 +31,39 @@ public class GraphViewController extends BaseScreenController {
     @FXML
     public void initialize() {
         super.initialize(mainPane,menuBarToggleGroup,graphViewButton,undoButton,redoButton);
+        ArrayList<Entry> entries = profile.getDiary().getAllEntries();
+
         final NumberAxis xAxis = new NumberAxis();
         final NumberAxis yAxis = new NumberAxis();
+
         xAxis.setLabel("Weeks");
+        xAxis.setMinorTickVisible(false);
+        xAxis.setAutoRanging(false);
+        xAxis.setLowerBound(0);
+        xAxis.setUpperBound(entries.size()+1);
+        xAxis.setTickUnit(1);
+
         yAxis.setLabel("Calories");
+        yAxis.setMinorTickVisible(false);
+        yAxis.setAutoRanging(false);
+        yAxis.setLowerBound(0);
+        yAxis.setUpperBound(2000);
+        yAxis.setTickUnit(200);
+
         final LineChart<Number,Number> lineChart = new LineChart<>(xAxis, yAxis);
 
         XYChart.Series series = new XYChart.Series();
 
-        series.getData().add(new XYChart.Data(1, 23));
-        series.getData().add(new XYChart.Data(2, 14));
-        series.getData().add(new XYChart.Data(3, 15));
-        series.getData().add(new XYChart.Data(4, 24));
-        series.getData().add(new XYChart.Data(5, 34));
-        series.getData().add(new XYChart.Data(6, 36));
-        series.getData().add(new XYChart.Data(7, 22));
-        series.getData().add(new XYChart.Data(8, 45));
-        series.getData().add(new XYChart.Data(9, 43));
-        series.getData().add(new XYChart.Data(10, 17));
-        series.getData().add(new XYChart.Data(11, 29));
-        series.getData().add(new XYChart.Data(12, 25));
+        for (int i = 0; i < entries.size(); i++) {
+            series.getData().add(new XYChart.Data(i + 1, entries.get(i).getNutrientValueForEntry("Energy (kcal)")));
+        }
 
         lineChart.getData().add(series);
         lineChart.setLayoutX(100);
         lineChart.setLayoutY(67);
         lineChart.setLegendVisible(false);
-        mainPane.getChildren().add(lineChart);
 
+        mainPane.getChildren().add(lineChart);
     }
 
 }
