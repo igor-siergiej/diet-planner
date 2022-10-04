1c4bea67b44f8ab80115b9c02349234a8dded227
igor-siergiej <igorsiergiej@gmail.com>
2022-10-04 12:53:26 +0100

    Created better input validator for float fields

--- src/com/app/planner/InputValidator.java
+++ src/com/app/planner/InputValidator.java
@@ -40,8 +40,16 @@ public class InputValidator {
         createEventHandler(passwordField, messageLabel, ValidatorType.PASSWORD, null, showPasswordTextField);
     }
 
+    public void createPasswordValidator(TextField showPasswordTextField, Label messageLabel, PasswordField passwordField) {
+        createEventHandler(showPasswordTextField, messageLabel, ValidatorType.PASSWORD, passwordField, null);
+    }
+
     public void createRetypePasswordValidator(PasswordField passwordField, PasswordField retypePasswordField, Label messageLabel, TextField showRetypePasswordTextField) {
         createEventHandler(retypePasswordField, messageLabel, ValidatorType.RETYPE, passwordField, showRetypePasswordTextField);
+    }
+
+    public void createRetypePasswordTextValidator(TextField showRetypePasswordTextField, TextField showPasswordTextField, Label messageLabel, PasswordField retypePasswordField) {
+        createEventHandler(showPasswordTextField, messageLabel, ValidatorType.RETYPE, retypePasswordField, showRetypePasswordTextField);
     }
 
     private void createEventHandler(TextField field, Label messageLabel, ValidatorType validatorType, PasswordField passwordField, TextField showPasswordTextField) {
@@ -61,18 +69,18 @@ public class InputValidator {
             if (validatorType == ValidatorType.EMAIL) {
                 System.out.println("Task Scheduled");
                 timer.schedule(createEmailTask(field, messageLabel), VALIDATION_DELAY);
-            } else  if (validatorType == ValidatorType.PASSWORD){
+            } else if (validatorType == ValidatorType.PASSWORD) {
                 System.out.println("Task Scheduled");
                 timer.schedule(createPasswordTask(field, messageLabel, showPasswordTextField), VALIDATION_DELAY);
             } else {
                 System.out.println("Task Scheduled");
-                timer.schedule(createRetypePasswordTask(field,messageLabel,passwordField, showPasswordTextField), VALIDATION_DELAY);
+                timer.schedule(createRetypePasswordTask(field, messageLabel, passwordField, showPasswordTextField), VALIDATION_DELAY);
             }
         });
     }
 
     private TimerTask createEmailTask(TextField textField, Label messageLabel) {
-        return getTimerTask(textField, messageLabel, ValidatorType.EMAIL, null,null);
+        return getTimerTask(textField, messageLabel, ValidatorType.EMAIL, null, null);
     }
 
     private TimerTask createPasswordTask(TextField passwordField, Label messageLabel, TextField showPasswordTextField) {
@@ -80,7 +88,7 @@ public class InputValidator {
     }
 
     private TimerTask createRetypePasswordTask(TextField passwordField, Label messageLabel, PasswordField retypePasswordField, TextField showRetypePasswordTextField) {
-        return getTimerTask(passwordField,messageLabel,ValidatorType.RETYPE, retypePasswordField, showRetypePasswordTextField);
+        return getTimerTask(passwordField, messageLabel, ValidatorType.RETYPE, retypePasswordField, showRetypePasswordTextField);
     }
 
     @NotNull
@@ -105,7 +113,7 @@ public class InputValidator {
                             messageLabel.setText(StringValidation.emailValidation(fieldText));
                             field.setId("text-field-error");
                         }
-                    } else if (validatorType == ValidatorType.PASSWORD){
+                    } else if (validatorType == ValidatorType.PASSWORD) {
                         if (StringValidation.passwordValidation(fieldText).equals(StringValidation.RETURN_STRING)) {
                             messageLabel.setId("correctLabel");
                             messageLabel.setText(StringValidation.RETURN_STRING);
